b645a374ad20813ff5ec6b906fe7cf78abfd1a65
Daniel <massao.tsuzuki@gmail.com>
2024-01-27 03:43:45 -0300

    Update category entity

--- src/main/java/danieltsuzuki/com/github/catalogo/entities/Category.java
+++ src/main/java/danieltsuzuki/com/github/catalogo/entities/Category.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -21,6 +18,7 @@ public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
     private String name;
 
 }
