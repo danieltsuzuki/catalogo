ed1ef16b1e39010d37dbf0a85476e08d9379f013
Daniel <massao.tsuzuki@gmail.com>
2024-01-27 13:44:05 -0300

    Update endpoint findAll

--- src/main/java/danieltsuzuki/com/github/catalogo/services/CategoryService.java
+++ src/main/java/danieltsuzuki/com/github/catalogo/services/CategoryService.java
@@ -7,12 +7,12 @@ import danieltsuzuki.com.github.catalogo.services.exceptions.DatabaseException;
 import danieltsuzuki.com.github.catalogo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -21,8 +21,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
-        return repository.findAll().stream().map(CategoryDTO::new).collect(Collectors.toList());
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+        Page<Category> list = repository.findAll(pageRequest);
+        return list.map(CategoryDTO::new);
     }
 
     @Transactional(readOnly = true)
