3129e7c12a43ba9084a7c04a366c0f413dcbe77a
Daniel <massao.tsuzuki@gmail.com>
2024-01-27 21:06:32 -0300

    Add product entity

---
+++ src/main/java/danieltsuzuki/com/github/catalogo/entities/Product.java
@@ -0,0 +1,44 @@
+package danieltsuzuki.com.github.catalogo.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private BigDecimal price;
+    private String imgUrl;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+    @ManyToMany
+    @JoinTable(
+            name = "product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+
+    public Product(Long id, String name, String description, BigDecimal price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+}
