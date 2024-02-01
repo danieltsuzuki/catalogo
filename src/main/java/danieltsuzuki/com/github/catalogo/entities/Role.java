a4c20d274977687c47b240d71147e3bc5f769ccd
Daniel <massao.tsuzuki@gmail.com>
2024-02-01 01:08:00 -0300

    Add role and user entity

---
+++ src/main/java/danieltsuzuki/com/github/catalogo/entities/Role.java
@@ -0,0 +1,24 @@
+package danieltsuzuki.com.github.catalogo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+}
