557103ce0f0823cbd3cbcfe0790f014ae290573b
Daniel <massao.tsuzuki@gmail.com>
2024-02-01 01:07:43 -0300

    Update category and product entity

--- src/main/java/danieltsuzuki/com/github/catalogo/entities/Category.java
+++ src/main/java/danieltsuzuki/com/github/catalogo/entities/Category.java
@@ -5,12 +5,15 @@ import lombok.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
     @Id
@@ -22,6 +25,8 @@ public class Category implements Serializable {
     private Instant createdAt;
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category(Long id, String name) {
         this.id = id;
