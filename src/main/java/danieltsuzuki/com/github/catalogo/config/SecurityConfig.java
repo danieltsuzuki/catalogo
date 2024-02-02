9587b72a1311b90df0220b7aec0c277de8f9fe51
Daniel <massao.tsuzuki@gmail.com>
2024-02-02 03:12:25 -0300

    Add security config

---
+++ src/main/java/danieltsuzuki/com/github/catalogo/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package danieltsuzuki.com.github.catalogo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                (requests) -> requests
+                        .anyRequest().permitAll()
+        ).csrf(csrf -> csrf.disable()).build();
+
+    }
+
+}
