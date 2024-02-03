679acae47f6967a77f1670047f7a0fa4a7696f2b
Daniel <massao.tsuzuki@gmail.com>
2024-02-03 14:35:33 -0300

    Add findByEmail in user repository

--- src/main/java/danieltsuzuki/com/github/catalogo/repositories/UserRepository.java
+++ src/main/java/danieltsuzuki/com/github/catalogo/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package danieltsuzuki.com.github.catalogo.repositories;
 
-import danieltsuzuki.com.github.catalogo.entities.Category;
 import danieltsuzuki.com.github.catalogo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
 }
